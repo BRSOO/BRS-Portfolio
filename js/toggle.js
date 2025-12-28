ed2c4306041df69670743acdb4b11828ee806451
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2025-12-28 21:13:35 +0545

    Made some changes to toggle

--- js/toggle.js
+++ js/toggle.js
@@ -2,6 +2,11 @@ const toggle = document.getElementById("toggle");
 const toggle_ball = document.getElementById("toggle-ball");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
+const navbar = document.getElementById("navbar");
+// let a = document.querySelectorAll("a");
+// a.forEach(a=>{
+//     a.style.color="black";
+// })
 
 toggle.addEventListener("click",()=>
 {
@@ -10,17 +15,23 @@ toggle.addEventListener("click",()=>
         toggle_ball.classList.remove("toggle-ball-on");
         toggle_ball.classList.add("toggle-ball-off");
         toggle.classList.toggle("toggle-on");
-        // document.body.style.backgroundColor="white";
+        //  document.body.style.backgroundColor="#3f3f3d";
+        document.body.classList.toggle("background-on");
+        document.body.classList.toggle("background-off");
         sun.style.display = "none";
         moon.style.display = "block";
+        navbar.classList.toggle("navbar-on");
     }
     else{
         toggle_ball.classList.remove("toggle-ball-off");
         toggle_ball.classList.add("toggle-ball-on");
         toggle.classList.toggle("toggle-on");
-        // document.body.style.backgroundColor="black";
+        // document.body.style.backgroundColor="white";
+        document.body.classList.toggle("background-on");
+        document.body.classList.toggle("background-off");
         moon.style.display = "none";
         sun.style.display = "block";
+        navbar.classList.toggle("navbar-on");
     }
     
 });
